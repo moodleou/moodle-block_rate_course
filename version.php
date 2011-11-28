1f5face5e61242ba9d66168defcc8458ad1e6e13
Jenny Gray <j.m.gray@open.ac.uk>
2011-11-28 10:55:44 +0000

    fixed plugin database validation errors

--- version.php
+++ version.php
@@ -2,4 +2,7 @@
 
 $plugin->version = 2011062301;
 
+$plugin->requires     = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0);
+$plugin->maturity     = MATURITY_BETA;
+$plugin->release      = '1.0';
 ?>
