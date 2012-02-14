048e07a05d5b945c7dc11ed814bddbff5a8fba29
Jenny Gray <j.m.gray@open.ac.uk>
2012-02-14 15:54:05 +0000

    Fix database error problem for users not integrating with questionnaire

--- lang/en/block_rate_course.php
+++ lang/en/block_rate_course.php
@@ -21,5 +21,5 @@ $string['rating_users'] ='Rated by {$a} user(s)';
 
 // questionnaire integration 
 $string['survey'] = 'What is the questionnaire name?';
-$string['survey_help'] = 'This is the questionnaire module which will be linked to';
+$string['survey_help'] = 'This is the questionnaire module which will be linked to (leave blank to stop link to questionnaire)';
 $string['viewreview'] = 'View questionnaire responses';
