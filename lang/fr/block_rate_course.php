005783027d1f5071903ed2d64245ff3966a43518
ben <benoit.works@gmail.com>
2018-03-01 14:53:47 +0100

    french translation

---
+++ lang/fr/block_rate_course.php
@@ -0,0 +1,50 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_rate_course', language 'fr', branch 'MOODLE_20_STABLE'
+ *
+ * @package    block
+ * @subpackage rate_course
+ * @copyright  2009 Jenny Gray
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
+ * Code was Rewritten for Moodle 2.X By Atar + Plus LTD for Comverse LTD.
+ * @copyright &copy; 2011 Comverse LTD.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ */
+
+$string ['rate_course: addinstance'] = 'Ajouter un nouveau bloc de notes de cours';
+$string ['rate_course: myaddinstance'] = 'évalue la capacité de mon écran';
+$string ['pluginname'] = 'Évaluation du cours';
+$string ['courserating'] = 'Évaluation du cours';
+
+$string ['completed'] = 'Merci. Vous avez déjà attribué une note à ce cours.';
+$string ['giverating'] = 'Donnez une note';
+$string ['intro'] = 'Veuillez indiquer à quel point vous avez apprécié ce cours.';
+$string ['submit'] = 'Soumettre ma note';
+$string ['noguestuseage'] = 'Pour évaluer ce cours, vous devrez vous connecter.';
+$string ['rate_course: rate'] = 'Donnez une note à un cours';
+$string ['rating_alt0'] = 'Évaluation du cours: Aucune note attribuée.';
+$string ['rating_altnum'] = 'Évaluation du cours : {$a} étoiles.';
+$string ['rating_users'] = 'Noté par {$a} utilisateur(s)';
+$string ['editingsitehome'] = 'Ce bloc affichera les notes sur les pages de cours seulement, et est caché ailleurs.';
+$string ['pleaselogin'] = 'Veuillez vous connecter pour noter cette ressource.';
+
+// Intégration du questionnaire.
+$string ['survey'] = 'Quel est le nom du questionnaire ?';
+$string ['survey_help'] = 'Ceci est le module du questionnaire qui sera lié (laisser vide pour arrêter le lien vers le questionnaire)';
+$string ['viewreview'] = 'Voir les réponses au questionnaire';
